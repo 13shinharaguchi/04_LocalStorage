08432a7f173d92525742e8b23e31b044e5983dad
13shinharaguchi <shin13usa@icloud.com>
2022-11-16 22:39:52 +0900

    一旦push

--- js/voice.js
+++ js/voice.js
@@ -9,6 +9,7 @@ $(".phonetic_match").on("click", function () {
     recognition.continuous = true;
     recognition.onresult = (event) => {
         const test2 = event.results[0][0].transcript
+
         console.log(test2)
         //入力されたものとあっているかを確認する
         if (test === test2) {
@@ -24,3 +25,34 @@ $(".phonetic_match").on("click", function () {
 $(".back_button").on("click", function () {
     location.href = "/html/login.html"
 })
+
+
+$(function () {
+    $("#btnAdd").val("追加").on('click', function () {
+
+        // 要素数
+        var n = $(".hoge").length + 1;
+
+        // 追加する要素文字列
+        var dom = "<div class='hoge' id='hoge-" + n + "'>" + n + ".";
+        dom += "<input type='text' id='hoge-in- " + n +"'/>";
+        dom += "</div>";
+
+        // 要素を追加
+        $("#dvContentArea").append(dom);
+    });
+    $("#btnDel").val("削除").on('click', function () {
+
+        // 要素数
+        var n = $(".hoge").length;
+
+        // 要素数が0の場合戻る
+        if (n <= 0) return;
+
+        // 要素の削除
+        $(`#hoge-${n}`).remove();
+    });
+});
+function changeInput(d) {
+    alert("id:" + d.id + "の値が" + $(d).val() + "に変更された。");
+}
