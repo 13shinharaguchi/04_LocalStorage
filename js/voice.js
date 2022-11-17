c890c314b07c590d0939fb48b71cd823493157a4
13shinharaguchi <shin13usa@icloud.com>
2022-11-17 22:42:53 +0900

    update

--- js/voice.js
+++ js/voice.js
@@ -56,3 +56,32 @@ $(function () {
 function changeInput(d) {
     alert("id:" + d.id + "の値が" + $(d).val() + "に変更された。");
 }
+
+$(function () {
+    // 削除処理
+    $("#delete").on("click", function () {
+        localStorage.clear();
+        showList();
+    });
+
+    // 保存処理
+    $("#save").on("click", function () {
+        localStorage.setItem(localStorage.length.toString(), $("#todo").val());
+        $("#todo").val("");
+        showList();
+    });
+
+    showList();
+});
+
+function showList() {
+    $("#list").html("");
+    for (let i = 0; i < localStorage.length; i++) {
+        $("#list").append("<li class='list-group-item'><button class='btn btn-danger mr-2' onclick='deleteItem(" + localStorage.key(i) + ")'><i class='fas fa-trash-alt'></i></button>" + localStorage.getItem(localStorage.key(i)) + "</li>");
+    }
+}
+
+function deleteItem(i) {
+    localStorage.removeItem(i);
+    showList();
+}
